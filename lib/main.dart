a910bb8993d780f848fcc6cba05af2e49a82fade
Marino Ricardo <74591655+marinoricardo@users.noreply.github.com>
2023-04-26 21:43:47 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:calculadora/views/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calculadora Idade',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
